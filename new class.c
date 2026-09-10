7eb4acd36d5a3454e64e815cb54615ee98c349a0
Ayush kumar verma <ayushkumarverma@Ayushs-MacBook-Air.local>
2026-09-11 01:00:39 +0530

    finding grade in c programming

---
+++ new class.c
@@ -0,0 +1,20 @@
+/*WRITE A CODE TO FIND THE GRATE THROUGH THE MARKS*/
+
+#include <stdio.h>
+int main(){
+    float score;
+
+    printf("enter score");
+    scanf("%f",&score);
+
+    if(score<40){
+        printf("fail");
+    }
+    else if (score >= 40 && score <= 60){
+        printf("c");
+    }
+    else { 
+        printf("best scorer");
+    }
+    return 0;
+}
