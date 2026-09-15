d268d428a930412d10b3ff39a53a30aae7a2777a
Ayush kumar verma <ayushkumarverma@Ayushs-MacBook-Air.local>
2026-09-15 15:32:30 +0530

    swith calculator

---
+++ newclass.c
@@ -0,0 +1,70 @@
+/*WAP TO CHECK WHEATHER A LETTEER OIS UPPER CASE OR LOWER CASE IN ENGLISH ALPHA BET*/
+/*
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a letter: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("UPPERCASE letter");
+    }
+    else if (ch >= 'a' && ch <= 'z') {
+        printf("LOWERCASE letter");
+    }
+    else {
+        printf("it is not An Alphabet letter");
+    }
+
+    return 0;
+}   */
+
+    /*FOR PARKING HOW MUCH MONEY FOR HOW MUCH TIME*/
+/*
+    #include <stdio.h>
+    int main() {
+        int fees;
+
+        printf("enter the time");
+        scanf("%d",&fees);
+
+        if (hr <= 3){
+        fees = 20;}
+        else if{
+            (hr <= 5)
+        fees = 50;  }
+        return 0;
+
+    }
+        */
+
+
+
+
+        #include <stdio.h>
+
+        int main() {
+            int var;
+            printf("ENTER THE AGE");
+            scanf("%d",&var);
+
+            switch (var){
+                case 15:
+                printf("YOU ARE A KID");
+                break;
+                case 18:
+                printf("ELIGIBLE FOR VOTE");
+                break;
+                default:
+                printf("Default Case is execuated");
+                break;
+
+
+            }
+            return 0;
+            
+        }
+
+        
