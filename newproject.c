eff85cf95e8ecad901da5e6ddcc938fb8b29f1fc
ayushkumarverma662@gmail.com <yaha-apna-verified-email-daalo@example.com>
2026-09-16 13:27:25 +0530

    Traffic Signals

---
+++ newproject.c
@@ -0,0 +1,27 @@
+ #include <stdio.h>
+
+    int main() {
+   char light;
+    
+    printf("enter the colour of the light");
+    scanf("%c",&light);
+
+    switch (light)
+    {
+    case 'Y' :
+    printf("you have to wait");
+        break;
+    case 'G':
+    printf("You can Go");
+    break;
+    case 'R' :
+    printf("Stop");
+    break;
+
+    default:
+    printf("invalid colour");
+        break;
+    }
+    return 0;
+
+}
